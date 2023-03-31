3b53d61fee8734e80c0de80bc39a1de96817fea4
Chelsy <chelsydhir@gmail.com>
2023-03-31 00:02:33 -0700

    CSIS3380: Quiz2

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+app = express();
+port = process.env.port || 7000
+
+mongoose.connect('mongodb+srv://chelsy:12345@nodeexpress-jwt-test.63qan.mongodb.net/Exam');
+
+const schema = new mongoose.Schema({
+    name: {type: String, required: true},
+    sid: {type: Number, required: true}
+});
+
+const Quiz = mongoose.model("Quiz", schema);
+app.get('/', (req, res) => {
+    
+    data = new Quiz({
+        name: "Chelsy",
+        sid: 300342149
+    });
+
+    data.save();
+    console.log("Data added successfully!");
+    res.send("Data added successfully!")
+})
+
+
+app.listen(port, () => {
+    console.log("App is listening at port");
+})
